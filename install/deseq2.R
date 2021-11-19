46dd71a3e88cd818c9b48d4e21a3ed679c9dc8ac
nictru <nictru32@gmail.com>
2021-11-19 17:07:59 +0100

    Add biomaRt R package installation

--- install/deseq2.R
+++ install/deseq2.R
@@ -3,4 +3,8 @@
 if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
-BiocManager::install("DESeq2")
+if (!requireNamespace("biomaRt", quietly = TRUE))
+    BiocManager::install("biomaRt")
+
+if (!requireNamespace("DESeq2", quietly = TRUE))
+    BiocManager::install("DESeq2")
