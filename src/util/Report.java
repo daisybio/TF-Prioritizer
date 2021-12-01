bf45ca8f36020d2ba70de43fb32ba54b7cb48d8f
nictru <nictru32@gmail.com>
2021-12-01 18:22:05 +0100

    Introduce Report class

---
+++ src/util/Report.java
@@ -0,0 +1,12 @@
+package util;
+
+public class Report
+{
+    public void generate()
+    {
+        System.out.println("[REPORT] Start generating report");
+
+
+        System.out.println("[REPORT] Finished generating report");
+    }
+}
