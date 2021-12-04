2c7db30dfa9ab44bc5f473188603e792a29ceeb8
nictru <nictru32@gmail.com>
2021-12-04 16:40:06 +0100

    Add TPM to HOME.html

--- src/util/Report.java
+++ src/util/Report.java
@@ -1,8 +1,11 @@
 package util;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.text.DecimalFormat;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Report
@@ -62,6 +65,7 @@ public class Report
         {
             StringBuilder sb_tfs = new StringBuilder();
             boolean firstLine = true;
+            DecimalFormat formatter = new DecimalFormat("0.000");
 
             File gene_symbol_map = new File(options_intern.com2pose_working_directory + File.separator +
                     options_intern.folder_name_deseq2_preprocessing + File.separator +
@@ -82,22 +86,71 @@ public class Report
                 String gene_symbol = line.split("\t")[1];
                 String ensg_symbol = "";
 
-                try (Scanner mapScanner = new Scanner(gene_symbol_map))
+                try
                 {
-                    while (mapScanner.hasNextLine())
+                    ensg_symbol = findValueInTable(gene_symbol, 1, 0, gene_symbol_map, "\t", true);
+                } catch (NoSuchFieldException ignored)
+                {
+                }
+
+                tf_string = tf_string.replace("{TF_NAME}", i + ". " + gene_symbol);
+
+                {   //LOG2FC
+                    File d_log2fs = new File(options_intern.com2pose_working_directory + File.separator +
+                            options_intern.folder_name_deseq2_output);
+
+                    StringBuilder sb_log2fc = new StringBuilder();
+
+                    for (File entry : Objects.requireNonNull(d_log2fs.listFiles()))
                     {
-                        String translation = mapScanner.nextLine();
-                        if (translation.split("\t").length == 2)
+                        if (entry.isFile())
                         {
-                            if (gene_symbol.equalsIgnoreCase(translation.split("\t")[1]))
+                            String group1 = entry.getName().split("_")[0];
+                            String group2 = entry.getName().split("_")[1];
+
+                            try
                             {
-                                ensg_symbol = translation.split("\t")[0];
+                                double log2fc =
+                                        Double.parseDouble(findValueInTable(ensg_symbol, 0, 1, entry, "\t", false));
+                                sb_log2fc.append(
+                                        "<p>" + group1 + " -> " + group2 + "</p><p>" + formatter.format(log2fc) +
+                                                "</p>");
+                            } catch (NoSuchFieldException ignored)
+                            {
                             }
                         }
                     }
-                }
-                tf_string = tf_string.replace("{TF_NAME}", i + ". " + gene_symbol);
-                tf_string = tf_string.replace("{CONTENT}", ensg_symbol);
+                    tf_string = tf_string.replace("{LOG2FC}", sb_log2fc.toString());
+                }   //LOG2FC
+
+                {   //TPM
+                    File d_tpm = new File(options_intern.com2pose_working_directory + File.separator +
+                            options_intern.folder_name_deseq2_preprocessing + File.separator +
+                            options_intern.folder_name_deseq2_preprocessing_tpm + File.separator +
+                            options_intern.folder_name_deseq2_preprocessing_tpm_results);
+
+                    StringBuilder sb_tpm = new StringBuilder();
+
+                    for (File entry : Objects.requireNonNull(d_tpm.listFiles()))
+                    {
+                        if (entry.isFile() && entry.getName().endsWith(".csv"))
+                        {
+                            String group = entry.getName().split("_")[0];
+
+                            try
+                            {
+                                double tpm =
+                                        Double.parseDouble(findValueInTable(ensg_symbol, 0, 3, entry, "\t", false));
+                                sb_tpm.append("<p>" + group + "</p><p>" + formatter.format(tpm) + "</p>");
+                            } catch (NoSuchFieldException ignored)
+                            {
+                            }
+                        }
+                    }
+                    tf_string = tf_string.replace("{TPM}", sb_tpm.toString());
+                }   //TPM
+
+                tf_string = tf_string.replace("{GENEID}", ensg_symbol);
                 sb_tfs.append(tf_string);
                 i++;
             }
@@ -107,6 +160,31 @@ public class Report
 
         writeFile(options_intern.com2pose_working_directory + File.separator + options_intern.f_out_report_home, home);
         logger.logLine("[REPORT] Finished generating report home page");
+    }
+
+    private String findValueInTable(String term, int searchIndex, int resultIndex, File file, String sep,
+                                    boolean ignoreCase) throws FileNotFoundException, NoSuchFieldException
+    {
+        if (file.isFile())
+        {
+            try (Scanner scanner = new Scanner(file))
+            {
+                while (scanner.hasNextLine())
+                {
+                    String line = scanner.nextLine();
+                    if (line.split(sep).length > searchIndex && line.split(sep).length > resultIndex)
+                    {
+
+                        if (term.equals(line.split(sep)[searchIndex]) ||
+                                ignoreCase && term.equalsIgnoreCase(line.split(sep)[searchIndex]))
+                        {
+                            return line.split(sep)[resultIndex];
+                        }
+                    }
+                }
+            }
+        }
+        throw new NoSuchFieldException();
     }
 
     private void styleAndScript() throws IOException
