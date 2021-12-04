83d0ee930be506467a4c62f334d236b025777269
nictru <nictru32@gmail.com>
2021-12-04 09:49:26 +0100

    Implement first report

--- src/util/Report.java
+++ src/util/Report.java
@@ -1,5 +1,9 @@
 package util;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
 public class Report
 {
     private final Logger logger;
@@ -14,7 +18,9 @@ public class Report
     public void generate() throws Exception
     {
         logger.logLine("[REPORT] Start generating report");
+
         generateHome();
+        styleAndScript();
 
         logger.logLine("[REPORT] Finished generating report");
     }
@@ -22,8 +28,51 @@ public class Report
     private void generateHome() throws Exception
     {
         logger.logLine("[REPORT] Start generating report home page");
-        logger.logLine(options_intern.com2pose_working_directory);
+        String home = loadFrame();
+        home = home.replace("{BODY}", loadFile(
+                options_intern.path_to_COM2POSE + File.separator + options_intern.f_report_resources_home_home_html));
+        home = home.replace("{TITLE}", "Overview");
+        home = home.replace("{TFS}", loadFile(
+                options_intern.path_to_COM2POSE + File.separator + options_intern.f_report_resources_home_tf_html));
 
+
+        writeFile(options_intern.com2pose_working_directory + File.separator + options_intern.f_out_report_home, home);
         logger.logLine("[REPORT] Finished generating report home page");
+    }
+
+    private void styleAndScript() throws IOException
+    {
+        logger.logLine("[REPORT] Start moving CSS and JS components");
+        String css =
+                loadFile(options_intern.path_to_COM2POSE + File.separator + options_intern.f_report_resources_style);
+        String script =
+                loadFile(options_intern.path_to_COM2POSE + File.separator + options_intern.f_report_resources_script);
+
+        writeFile(options_intern.com2pose_working_directory + File.separator + options_intern.f_out_report_style, css);
+        writeFile(options_intern.com2pose_working_directory + File.separator + options_intern.f_out_report_script,
+                script);
+    }
+
+    private void writeFile(String path, String content) throws IOException
+    {
+        File target = new File(path);
+        if (!target.exists())
+        {
+            target.getParentFile().mkdirs();
+            target.createNewFile();
+        }
+        Files.writeString(target.toPath(), content);
+    }
+
+    private String loadFrame() throws IOException
+    {
+        return loadFile(
+                options_intern.path_to_COM2POSE + File.separator + options_intern.f_report_resources_frame_html);
+    }
+
+    private String loadFile(String path) throws IOException
+    {
+        File source = new File(path);
+        return Files.readString(source.toPath());
     }
 }
