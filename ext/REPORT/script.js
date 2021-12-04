83fff3521d6d1efde192c759ed308bbd7610739f
nictru <nictru32@gmail.com>
2021-12-04 09:51:27 +0100

    Change var to let in javascript

--- ext/REPORT/script.js
+++ ext/REPORT/script.js
@@ -1,6 +1,6 @@
 function accordion() {
-    var acc = document.getElementsByClassName("accordion");
-    var i;
+    let acc = document.getElementsByClassName("accordion");
+    let i;
 
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
@@ -9,7 +9,7 @@ function accordion() {
             this.classList.toggle("active");
 
             /* Toggle between hiding and showing the active panel */
-            var panel = this.nextElementSibling;
+            let panel = this.nextElementSibling;
             if (panel.style.display === "block") {
                 panel.style.display = "none";
             } else {
