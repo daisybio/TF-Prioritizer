8ff436100836060d0f9118d4f87b12586d0b4fe1
nictru <nictru32@gmail.com>
2021-12-07 21:42:49 +0100

    Remove comments from script.js

--- ext/REPORT/script.js
+++ ext/REPORT/script.js
@@ -4,11 +4,8 @@ function accordion() {
 
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
-            /* Toggle between adding and removing the "active" class,
-            to highlight the button that controls the panel */
             this.classList.toggle("active");
 
-            /* Toggle between hiding and showing the active panel */
             let panel = this.nextElementSibling;
             if (panel.style.display === "grid") {
                 panel.style.display = "none";
