409d1a98fa861b5ada1ee14a17619980c40a8b92
nictru <nictru32@gmail.com>
2021-12-21 17:50:21 +0100

    Implement image switching

--- ext/REPORT/script.js
+++ ext/REPORT/script.js
@@ -15,3 +15,29 @@ function accordion() {
         });
     }
 }
+
+function selector() {
+    let selectors = document.getElementsByClassName("selector");
+    let i;
+
+    for (i = 0; i < selectors.length; i++) {
+        selectors[i].addEventListener("click", function () {
+            let setClasses = !this.classList.contains("active");
+            setClass(selectors, "active", "remove");
+
+            let image = document.getElementById(this.name)
+
+            image.src = this.value + ".png";
+
+            if (setClasses) {
+                this.classList.toggle("active");
+            }
+        });
+    }
+}
+
+function setClass(els, className, fnName) {
+    for (let i = 0; i < els.length; i++) {
+        els[i].classList[fnName](className);
+    }
+}
