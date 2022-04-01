3034e36168404892c872c28f0774e8cc4480cb35
nictru <nictru32@gmail.com>
2022-04-01 08:12:34 +0200

    Separate ArgParser from TFPRIO

---
+++ src/tfprio/ArgParser.java
@@ -0,0 +1,49 @@
+package tfprio;
+
+import org.apache.commons.cli.*;
+
+import java.io.File;
+
+public class ArgParser
+{
+    static void parseArguments(String[] args)
+    {
+        Options options = new Options();
+
+        Option opt_configFile = new Option("c", "com2pose-config", true,
+                "[REQ]: TFPRIO config file. Example in /TFPRIO/config_templates/configsTemplate.json");
+        opt_configFile.setRequired(true);
+        options.addOption(opt_configFile);
+
+        Option opt_workingDirectory = new Option("w", "working-directory", true,
+                "[REQ]: working directory where TFPRIO can create, remove and edit files");
+        opt_workingDirectory.setRequired(true);
+        options.addOption(opt_workingDirectory);
+
+        Option opt_tfprioDirectory = new Option("p", "path-tfprio", true, "[REQ]: filepath to TFPRIO folder");
+        opt_tfprioDirectory.setRequired(true);
+        options.addOption(opt_tfprioDirectory);
+
+
+        CommandLineParser parser = new DefaultParser();
+        HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd;
+
+        try
+        {
+            cmd = parser.parse(options, args);
+
+            TFPRIO.configFile = new File(cmd.getOptionValue("com2pose-config"));
+            TFPRIO.workingDirectory = new File(cmd.getOptionValue("working-directory"));
+            TFPRIO.tfprioDirectory = new File(cmd.getOptionValue("path-tfprio"));
+
+        } catch (ParseException e)
+        {
+            System.out.println(e.getMessage());
+            formatter.printHelp(
+                    "-c <com2pose-config> -w <working-directory> -p <path-com2pose> [-t <tgen-dir>] [-l] [-m] [-a] [-t] [-b]",
+                    options);
+            System.exit(1);
+        }
+    }
+}
--- src/tfprio/TFPRIO.java
+++ src/tfprio/TFPRIO.java
@@ -26,14 +26,14 @@ public class TFPRIO
     public static Config<File> latestInputDirectory;
     public static Set<String> existingHms = new HashSet<>();
 
-    private static File workingDirectory;
-    private static File tfprioDirectory;
-    private static File configFile;
+    static File workingDirectory;
+    static File tfprioDirectory;
+    static File configFile;
 
     public static void main(String[] args) throws Exception
     {
         ExecutionTimeMeasurement timer = new ExecutionTimeMeasurement();
-        parseArguments(args);
+        ArgParser.parseArguments(args);
 
         configs = new Configs(workingDirectory, tfprioDirectory);
 
@@ -55,47 +55,5 @@ public class TFPRIO
         */
 
         System.out.println("TFPRIO finished. Execution took " + timer.stopAndGetDeltaSeconds() + " seconds.");
-    }
-
-
-    private static void parseArguments(String[] args)
-    {
-        Options options = new Options();
-
-        Option opt_configFile = new Option("c", "com2pose-config", true,
-                "[REQ]: TFPRIO config file. Example in /TFPRIO/config_templates/configsTemplate.json");
-        opt_configFile.setRequired(true);
-        options.addOption(opt_configFile);
-
-        Option opt_workingDirectory = new Option("w", "working-directory", true,
-                "[REQ]: working directory where TFPRIO can create, remove and edit files");
-        opt_workingDirectory.setRequired(true);
-        options.addOption(opt_workingDirectory);
-
-        Option opt_tfprioDirectory = new Option("p", "path-tfprio", true, "[REQ]: filepath to TFPRIO folder");
-        opt_tfprioDirectory.setRequired(true);
-        options.addOption(opt_tfprioDirectory);
-
-
-        CommandLineParser parser = new DefaultParser();
-        HelpFormatter formatter = new HelpFormatter();
-        CommandLine cmd;
-
-        try
-        {
-            cmd = parser.parse(options, args);
-
-            TFPRIO.configFile = new File(cmd.getOptionValue("com2pose-config"));
-            TFPRIO.workingDirectory = new File(cmd.getOptionValue("working-directory"));
-            TFPRIO.tfprioDirectory = new File(cmd.getOptionValue("path-tfprio"));
-
-        } catch (ParseException e)
-        {
-            System.out.println(e.getMessage());
-            formatter.printHelp(
-                    "-c <com2pose-config> -w <working-directory> -p <path-com2pose> [-t <tgen-dir>] [-l] [-m] [-a] [-t] [-b]",
-                    options);
-            System.exit(1);
-        }
     }
 }
