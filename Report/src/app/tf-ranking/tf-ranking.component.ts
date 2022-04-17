bf34fb1972caacb919d84ea769f50ea5cc41a915
nictru <nictru32@gmail.com>
2022-04-17 20:53:24 +0200

    Implement search functionalities

--- Report/src/app/tf-ranking/tf-ranking.component.ts
+++ Report/src/app/tf-ranking/tf-ranking.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {TranscriptionFactorGroup} from "../types/types";
 import {TfDataGetterService} from "../services/tf-data-getter.service";
+import {resourceChangeTicket} from "@angular/compiler-cli/src/ngtsc/core";
 
 @Component({
   selector: 'tf-ranking',
@@ -16,5 +17,14 @@ export class TfRankingComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  hasTargetGene(term: string, tfGroup: TranscriptionFactorGroup) {
+    for (let targetGene of tfGroup.targetGenes) {
+      if (targetGene.geneID.toUpperCase().includes(term.toUpperCase()) || targetGene.symbol.toUpperCase().includes(term.toUpperCase())) {
+        return true;
+      }
+    }
+    return false;
   }
 }
