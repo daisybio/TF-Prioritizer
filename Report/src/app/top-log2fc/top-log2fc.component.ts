1343023704fcb13d2131c4843c4924563382daa8
nictru <nictru32@gmail.com>
2022-04-20 13:20:15 +0200

    Report improvement

---
+++ Report/src/app/top-log2fc/top-log2fc.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit} from '@angular/core';
+import {TfDataGetterService} from "../services/tf-data-getter.service";
+
+@Component({
+  selector: 'app-top-log2fc',
+  templateUrl: './top-log2fc.component.html',
+  styleUrls: ['./top-log2fc.component.css']
+})
+export class TopLog2fcComponent implements OnInit {
+  data: {};
+
+  constructor(private dataGetter: TfDataGetterService) {
+    this.data = dataGetter.getData().topLog2fc;
+  }
+
+  ngOnInit(): void {
+  }
+}
