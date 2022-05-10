be60c60360da819e1ac5304f55198c456e78db15
nictru <nictru32@gmail.com>
2022-05-10 09:00:30 +0200

    Improve stats formatting

--- scripts/logDockerStats.sh
+++ scripts/logDockerStats.sh
@@ -4,5 +4,6 @@ touch {STATSFILE}
 echo "TIME\tCONTAINER_ID\tCONTAINER_NAME\tCPU_PERCENTAGE\tMEMORY_USAGE\tMEMORY_PERCENTAGE\tPIDs" >> {STATSFILE}
 while true; do
   sleep 5;
-  docker stats --no-stream {CONTAINER_ID} | sed -n 2p | sed "s/^/$(date)\t/" | cat >> {STATSFILE}
+  docker stats --no-stream --format "table {{.ID}}\t{{.Name}}\t{{.CPUPerc}}\t{{.MemUsage}}\t{{.MemPerc}}\t{{.PIDs}}" \
+  {CONTAINER_ID} | sed -n 2p | sed "s/^/$(date)\t/" | cat >> {STATSFILE}
 done
