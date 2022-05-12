dfce463f1c0147b6d4ec7aee9425a81012015ab9
nictru <nictru32@gmail.com>
2022-05-12 22:40:34 +0200

    Fix glmnet installation

--- install/r_dependencies.R
+++ install/r_dependencies.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
 install.packages(c("BiocManager", "pheatmap", "devtools"))
-
+library(devtools)
 BiocManager::install(c("biomaRt", "data.table", "DESeq2", "ggplot2", "dplyr", "gplots", "doMC", "methods",
                        "DiffLogo", "seqLogo"), version = "3.15")
 install_version("glmnet", version = "4.1.3")
