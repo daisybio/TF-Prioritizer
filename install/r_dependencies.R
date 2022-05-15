489596465de2688105588fab6031fc6a2ede0888
nictru <nictru32@gmail.com>
2022-05-15 16:02:13 +0200

    Fix igv installation

--- install/r_dependencies.R
+++ install/r_dependencies.R
@@ -4,4 +4,3 @@ install.packages(c("BiocManager", "pheatmap", "devtools"))
 library(devtools)
 BiocManager::install(c("biomaRt", "data.table", "DESeq2", "ggplot2", "dplyr", "gplots", "doMC", "methods",
                        "DiffLogo", "seqLogo"), version = "3.15")
-install_version("glmnet", version = "4.1.3")
