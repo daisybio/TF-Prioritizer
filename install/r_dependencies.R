26f53cbdb086d4deb9f1bbb65bf88a4e222ab4a1
nictru <nictru32@gmail.com>
2022-05-17 12:50:59 +0200

    Remove outdated devtools installation

--- install/r_dependencies.R
+++ install/r_dependencies.R
@@ -1,6 +1,5 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("BiocManager", "pheatmap", "devtools"))
-library(devtools)
+install.packages(c("BiocManager", "pheatmap"))
 BiocManager::install(c("biomaRt", "data.table", "DESeq2", "ggplot2", "dplyr", "gplots", "doMC", "methods",
                        "DiffLogo", "seqLogo"), version = "3.15")
