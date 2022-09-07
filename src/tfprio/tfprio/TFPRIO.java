e56b69303cfffad5ee2242e58e6923451088c9f0
nictru <nictru32@gmail.com>
2022-09-07 12:02:30 +0200

    Fix workflow

--- src/tfprio/tfprio/TFPRIO.java
+++ src/tfprio/tfprio/TFPRIO.java
@@ -22,7 +22,7 @@ import static util.FileManagement.extend;
 
 public class TFPRIO
 {
-    public static final boolean developmentMode = true;
+    public static final boolean developmentMode = false;
     /**
      * The {@link Configs} object which is referenced by all the {@link lib.ExecutableStep} instances
      */
--- src/tfprio/tfprio/Workflow.java
+++ src/tfprio/tfprio/Workflow.java
@@ -25,7 +25,6 @@ public class Workflow
         }
 
         steps.add(new tfprio.InitStaticVariables());
-        /*
         steps.add(new lib.CheckChromosomes());
 
         if (TFPRIO.configs.mixOptions.level.isSet())
@@ -194,11 +193,10 @@ public class Workflow
                 notExecutedSteps.add(notExecutedStep);
             }
         }
-         */
 
         steps.add(new lib.ConfusionMatrixes.Generate());
 
-        /*steps.add(new lib.Igv.DcgTargetGenes());
+        steps.add(new lib.Igv.DcgTargetGenes());
 
         if (TFPRIO.configs.igv.importantLociAllPrioTf.isSet())
         {
@@ -220,7 +218,7 @@ public class Workflow
         }
         steps.add(new lib.DistributionAnalysis.CoOccurrenceAnalysis());
 
-        //steps.add(new lib.Report.Report());*/
+        //steps.add(new lib.Report.Report());
 
         steps.add(new lib.AngularReport.Generate());
         steps.add(new lib.AngularReport.PostProcessing());
