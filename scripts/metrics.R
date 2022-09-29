35b25b6e8c3718b1fcfec912e471dec474c72918
nictru <nictru32@gmail.com>
2022-09-29 08:41:38 +0200

    Implement color blind friendly metrics plot

--- scripts/metrics.R
+++ scripts/metrics.R
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(reshape2)
 
+safe_colorblind_palette <- c("#000000", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+
 stats<-read.csv("{INPUTFILE}",sep="\t")
 
 stats_visualized <- stats
@@ -11,6 +13,9 @@ stats_visualized$FN<-NULL
 
 stats_visualized<-melt(stats_visualized, id="TF",variable.name = "Metric",value.name = "Percentage")
 
-ggplot(stats_visualized, aes(fill=Metric, y=Percentage, x=TF)) +
+p <- ggplot(stats_visualized, aes(fill=Metric, y=Percentage, x=TF)) +
     geom_bar(position="dodge", stat="identity")
+
+p + scale_fill_manual(values=safe_colorblind_palette) + theme_bw()
+
 ggsave("{OUTPUTFILE}", width=nrow(stats_visualized)/2, units="cm")
