2312f4b02058799aa0b7de1d519dbdd474384b95
nictru <nictru32@gmail.com>
2023-01-15 08:31:21 +0100

    Fix matplotlib cache warnings

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,9 @@ FROM ubuntu:22.04
 
 LABEL org.opencontainers.image.source=https://github.com/biomedbigdata/TF-Prioritizer
 
+ENV DEBIAN_FRONTEND=noninteractive
 ENV RGTDATA=/srv/dependencies/rgtdata
+ENV MPLCONFIGDIR=/srv/dependencies/matplotlib
 
 COPY environment /srv/environment
 RUN chmod u+x /srv/environment/setup.sh && ./srv/environment/setup.sh
--- environment/setup.sh
+++ environment/setup.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-export DEBIAN_FRONTEND=noninteractive
-
 DIRECTORY=$(dirname "$0")
+
+# bedtools: Required by TEPIC
+# unzip: Required for ChipAtlas download extraction
+# xvfb: Required for headless IGV
+# wget: Required for HINT genome download
+# libcurl4-openssl-dev, libxml2-dev, r-cran-httr: Required by R packages
 
 apt-get update && apt-get install -y \
   openjdk-17-jre-headless \
@@ -16,8 +20,14 @@ apt-get update && apt-get install -y \
   wget \
   libcurl4-openssl-dev libxml2-dev r-cran-httr
 
+# Setup matplotlib cache directory
+mkdir -p "$MPLCONFIGDIR"
+chmod -R 777 "$MPLCONFIGDIR"
+
+# Install python packages, RGTDATA=Cache directory for HINT
 mkdir -p "$RGTDATA"
 python3 -m pip install -r "$DIRECTORY"/python_dependencies.txt
 chmod -R 777 "$RGTDATA"
 
+# Install R packages
 Rscript "$DIRECTORY"/setup.R
