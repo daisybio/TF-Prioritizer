378566b332c31570b9707242e81add2d7c5d5429
nictru <nictru32@gmail.com>
2023-01-15 09:59:43 +0100

    Handle empty predicted logo files

--- src/main/resources/org/exbio/tfprio/steps/logos/predicted.R
+++ src/main/resources/org/exbio/tfprio/steps/logos/predicted.R
@@ -10,11 +10,14 @@ parser$add_argument("--plot_file", required = TRUE)
 parser$add_argument("--output_file", required = TRUE)
 args <- parser$parse_args()
 
-currentPwm <- getPwmFromFastaFile(args$input_file)
-currentPwm <- seqLogo::makePWM(currentPwm)
-png(file=args$plot_file)
-seqLogo::seqLogo(currentPwm,fill=c(A="#4daf4a", C="#377eb8", G="#ffd92f", T="#e41a1c"))
-dev.off()
-currentPwm <- t(pwm(currentPwm))
-write.table(currentPwm, args$output_file, row.names = FALSE, col.names=FALSE, quote = F, sep="\t")
-
+if (file.size(args$input_file) == 0L) {
+    print(paste0("Empty file: ", args$input_file))
+} else {
+    currentPwm <- getPwmFromFastaFile(args$input_file)
+    currentPwm <- seqLogo::makePWM(currentPwm)
+    png(file=args$plot_file)
+    seqLogo::seqLogo(currentPwm,fill=c(A="#4daf4a", C="#377eb8", G="#ffd92f", T="#e41a1c"))
+    dev.off()
+    currentPwm <- t(pwm(currentPwm))
+    write.table(currentPwm, args$output_file, row.names = FALSE, col.names=FALSE, quote = F, sep="\t")
+}
