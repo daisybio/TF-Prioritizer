628bdaeefcb3be03cef70f040d46aefd8a7305e2
nictru <nictru32@gmail.com>
2023-01-17 21:53:28 +0100

    Recreation of angular project

--- src/main/resources/org/exbio/tfprio/steps/report/angular/README.md
+++ src/main/resources/org/exbio/tfprio/steps/report/angular/README.md
@@ -1,4 +1,4 @@
-# Report
+# Angular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.1.3.
 
--- src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/app.component.spec.ts
+++ src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
       declarations: [
         AppComponent
       ],
@@ -16,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'report'`, () => {
+  it(`should have as title 'angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('report');
+    expect(app.title).toEqual('angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('report app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular app is running!');
   });
 });
