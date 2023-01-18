927c715e5c233f0f45148525556bc2e1311d95b2
nictru <nictru32@gmail.com>
2023-01-18 08:40:03 +0100

    Update packages

--- src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/app.module.ts
+++ src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatExpansionModule} from "@angular/material/expansion";
 import { RankingComponent } from './pages/ranking/ranking.component';
-import {MatButtonModule} from "@angular/material/button";
-import {MatAutocompleteModule} from "@angular/material/autocomplete";
-import {MatInputModule} from "@angular/material/input";
-import {MatChipsModule} from "@angular/material/chips";
+import {MatLegacyButtonModule as MatButtonModule} from "@angular/material/legacy-button";
+import {MatLegacyAutocompleteModule as MatAutocompleteModule} from "@angular/material/legacy-autocomplete";
+import {MatLegacyInputModule as MatInputModule} from "@angular/material/legacy-input";
+import {MatLegacyChipsModule as MatChipsModule} from "@angular/material/legacy-chips";
 
 @NgModule({
   declarations: [
