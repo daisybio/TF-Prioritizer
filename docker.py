5c34afeef5c543c70e6233863b2475f1d5c21a25
Nico Trummer <52698566+nictru@users.noreply.github.com>
2023-02-01 10:22:33 +0100

    Update docker.py

--- docker.py
+++ docker.py
@@ -29,7 +29,7 @@ def process_configs(configs: dict) -> dict:
     return mounts
 
 
-image = args.i if args.i is not None else "nicotru/tf-prioritizer"
+image = args.i if args.i is not None else "nicotru/tf-prioritizer:master"
 config_path = os.path.abspath(args.c)
 threads = args.t
 output = os.path.abspath(args.o)
