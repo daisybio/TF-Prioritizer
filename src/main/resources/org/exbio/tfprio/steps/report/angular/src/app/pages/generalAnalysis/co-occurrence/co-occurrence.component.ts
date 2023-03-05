174dcc1d154ec3ab5a22cfa095867a19fc749d65
nictru <nictru32@gmail.com>
2023-03-05 18:43:52 +0100

    Improve cooccurence visualization for large numbers of tfs

--- src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/pages/generalAnalysis/co-occurrence/co-occurrence.component.ts
+++ src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/pages/generalAnalysis/co-occurrence/co-occurrence.component.ts
@@ -11,13 +11,30 @@ export class CoOccurrenceComponent {
   coOccurrence = this.dataService.coOccurrence;
   initial = 'relative';
   $valueType: Subject<string> = new Subject<string>();
+  $rawData: Subject<{ name: string, series: { name: string, value: number }[] }[]> = new Subject<{ name: string, series: { name: string, value: number }[] }[]>();
+  rawData!: { name: string, series: { name: string, value: number }[] }[];
+  $heatMapData: Subject<{ name: string, series: { name: string, value: number }[] }[]> = new Subject<{ name: string, series: { name: string, value: number }[] }[]>();
+  existingTfs: Set<string> = new Set<string>();
 
-  heatmapData: Subject<any> = new Subject<any>();
+  activeTfs: Set<string> = new Set<string>();
 
   constructor(private dataService: DataManagerService) {
+    this.$rawData.subscribe(data => {
+      this.rawData = data;
+      this.updateHeatMapData();
+    });
+
+    this.coOccurrence.forEach(tfObject => {
+      let tfName = tfObject.name;
+      this.existingTfs.add(tfName);
+      if (this.activeTfs.size < 17) {
+        this.activeTfs.add(tfName);
+      }
+    });
+
     this.$valueType.subscribe(type => {
       if (type === 'absolute') {
-        this.heatmapData.next(this.coOccurrence);
+        this.$rawData.next(this.coOccurrence);
       } else if (type === 'relative') {
         let data = [];
 
@@ -41,12 +58,37 @@ export class CoOccurrenceComponent {
           });
         }
 
-        this.heatmapData.next(data);
+        this.$rawData.next(data);
 
       } else {
         console.log('Unknown value type: ' + type);
       }
     })
+  }
+
+  toggleTf(tf: string) {
+    if (this.activeTfs.has(tf)) {
+      this.activeTfs.delete(tf);
+    } else {
+      this.activeTfs.add(tf);
+    }
+
+    this.updateHeatMapData();
+  }
+
+  updateHeatMapData() {
+    let data: { name: string, series: { name: string, value: number }[] }[] = [];
+    this.activeTfs.forEach(tf => {
+      let tfObject = this.rawData.filter(tfObject => tfObject.name === tf)[0];
+      let tfSeries = tfObject.series;
+      let series = tfSeries.filter(series => this.activeTfs.has(series.name) && series.name !== tf);
+      data.push({
+        name: tf,
+        series: series
+      });
+    });
+
+    this.$heatMapData.next(data);
   }
 
   ngAfterViewInit() {
