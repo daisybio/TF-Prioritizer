fe99f4234b805e0a89aba1825e35f18353eafcfa
lonman99 <leon-schwartz@web.de>
2023-03-27 10:10:03 +0200

    added R dependencies for ehmm

--- environment/setup.R
+++ environment/setup.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("BiocManager", "pheatmap", "argparse", "glmnet"))
+install.packages(c("BiocManager", "pheatmap", "argparse", "glmnet", "devtools"))
 BiocManager::install(c("biomaRt", "data.table", "DESeq2", "ggplot2", "dplyr", "gplots", "doMC", "methods",
-                       "DiffLogo", "seqLogo"), version = "3.14")
+                       "DiffLogo", "seqLogo", "IRanges", "GenomicRanges", "bamsignals", "rtracklayer",
+                       "Rsamtools", "edgeR", "affyPLM"), version = "3.14")
+devtools::install_github("tobiaszehnder/ehmm")
