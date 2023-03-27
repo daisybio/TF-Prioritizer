33747609657e2784e1bb2b59288e700163962b3a
lonman99 <leon-schwartz@web.de>
2023-03-27 12:52:41 +0200

    Integrated EHMM config module

---
+++ src/main/java/org/exbio/tfprio/configs/modules/EHMM.java
@@ -0,0 +1,16 @@
+package org.exbio.tfprio.configs.modules;
+
+import org.exbio.pipejar.configs.ConfigModule;
+import org.exbio.pipejar.configs.ConfigTypes.InputTypes.ExternalConfig;
+import org.exbio.pipejar.configs.ConfigTypes.InputTypes.InternalConfig;
+
+import java.io.File;
+
+
+public class EHMM extends ConfigModule {
+    public final ExternalConfig<File> bam_directory = new ExternalConfig<>(File.class);
+    public final InternalConfig<Integer> nStates = new InternalConfig<>(10);
+    public final InternalConfig<Double> pseudoCount = new InternalConfig<>(1e-6);
+    public final InternalConfig<Integer> nBins = new InternalConfig<>(10);
+    public final InternalConfig<Integer> nThreads = new InternalConfig<>(1);
+}
