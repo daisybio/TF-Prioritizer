d09a21417cab7c330fd4baaa09c786bb5e48e5c6
nictru <nictru32@gmail.com>
2023-03-28 12:45:23 +0200

    Proof of concept

--- src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/pages/ranking/ranking.component.ts
+++ src/main/resources/org/exbio/tfprio/steps/report/angular/src/app/pages/ranking/ranking.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {tfGroup} from "../../../interfaces";
 import {DataManagerService} from "../../services/data-manager.service";
 import {Subject} from "rxjs";
@@ -11,6 +11,7 @@ import {FormControl} from "@angular/forms";
 })
 export class RankingComponent implements OnInit {
   filterControl = new FormControl('');
+  @ViewChild('igvDiv') igvDiv!: ElementRef;
   tfGroups: tfGroup[] = this.dataService.groups;
   hmList: string[] = this.dataService.hms;
   hmActive: Map<string, boolean> = new Map<string, boolean>(
@@ -37,6 +38,26 @@ export class RankingComponent implements OnInit {
     setTimeout(() => {
       this.updateVisibleGroups(null);
     }, 0);
+
+    setTimeout(() => {
+      const options =
+        {
+          genome: "hg38",
+          locus: "chr8:127,736,588-127,739,371",
+          tracks: [
+            {
+              "name": "HG00103",
+              "url": "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram",
+              "indexURL": "https://s3.amazonaws.com/1000genomes/data/HG00103/alignment/HG00103.alt_bwamem_GRCh38DH.20150718.GBR.low_coverage.cram.crai",
+              "format": "cram"
+            }
+          ]
+        };
+
+      console.log(this.igvDiv);
+      // @ts-ignore
+      igv.createBrowser(this.igvDiv.nativeElement, options);
+    });
   }
 
 
