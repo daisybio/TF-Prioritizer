4d8b040dd1f613ba31060dc2af522d747c56120d
lonman99 <leon-schwartz@web.de>
2023-04-04 15:28:36 +0200

    Im dumb, added argparse dependency

--- environment/setup.R
+++ environment/setup.R
@@ -1,6 +1,6 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("BiocManager", "pheatmap", "argparser", "glmnet"))
+install.packages(c("BiocManager", "pheatmap", "argparse", "glmnet", "argparser"))
 BiocManager::install(c("biomaRt", "data.table", "DESeq2", "ggplot2", "dplyr", "gplots", "doMC", "methods",
                        "DiffLogo", "seqLogo", "IRanges", "GenomicRanges", "bamsignals", "rtracklayer",
                        "Rsamtools", "edgeR", "affyPLM"), version = "3.14")
