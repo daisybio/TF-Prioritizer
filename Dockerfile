50dc966250debb89148f32345ee2f6c7af2dd8a0
lonman99 <leon-schwartz@web.de>
2023-04-04 12:40:16 +0200

    Added ehmm R package as local package

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,7 @@ COPY environment /srv/environment
 RUN chmod u+x /srv/environment/setup.sh && ./srv/environment/setup.sh
 RUN mkdir -p "/srv/temp" && chmod -R 777 "/srv/temp"
 
+COPY lib/ehmm-master.zip /srv/dependencies/ehmm-master.zip
+RUN R -e "install.packages('/srv/dependencies/ehmm-master.zip', repos = NULL, type = 'source')"
+
 COPY bin/TF-Prioritizer.jar /srv/TF-Prioritizer.jar
--- environment/setup.R
+++ environment/setup.R
@@ -1,7 +1,6 @@
 #!/usr/bin/env Rscript
 
-install.packages(c("BiocManager", "pheatmap", "argparse", "glmnet", "devtools"))
+install.packages(c("BiocManager", "pheatmap", "argparse", "glmnet"))
 BiocManager::install(c("biomaRt", "data.table", "DESeq2", "ggplot2", "dplyr", "gplots", "doMC", "methods",
                        "DiffLogo", "seqLogo", "IRanges", "GenomicRanges", "bamsignals", "rtracklayer",
                        "Rsamtools", "edgeR", "affyPLM"), version = "3.14")
-devtools::install_github("tobiaszehnder/ehmm")
