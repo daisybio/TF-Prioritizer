56a3128ce16c605acad01dc712f52a0124c60d18
lonman99 <leon-schwartz@web.de>
2023-04-25 18:59:44 +0200

    Keep complex predicted ranges

--- src/main/resources/org/exbio/tfprio/steps/ehmm/combinePredictions.R
+++ src/main/resources/org/exbio/tfprio/steps/ehmm/combinePredictions.R
@@ -6,6 +6,7 @@ args <- commandArgs(trailingOnly = T)
 parser <- arg_parser("EHMM command line parser", name = "ehmm")
 parser <- add_argument(parser, "-d", help = "Directory containing applyModel output")
 parser <- add_argument(parser, "-o", help = "Output directory")
+parser <- add_argument(parser, "-t", help = "Filtering ehmm score threshold", default = 0.9)
 argv <- parse_args(parser, argv = args)
 # find enhancer and promoter files
 promoterFiles <- list.files(argv$d, pattern = "promoterRegions.bed", recursive = T, full.names = T)
@@ -13,6 +14,9 @@ enhancerFiles <- list.files(argv$d, pattern = "enhancerRegions.bed", recursive =
 # import all ranges
 promoterRegions <- sort(do.call(c, lapply(promoterFiles, import, format = "bed")))
 enhancerRegions <- sort(do.call(c, lapply(enhancerFiles, import, format = "bed")))
+# filter regions for score threshold
+promoterRegions <- promoterRegions[promoterRegions$score >= argv$t]
+enhancerRegions <- enhancerRegions[enhancerRegions$score >= argv$t]
 # write files to output
-export(reduce(promoterRegions), file.path(argv$o, "predictedPromoters.bed"), format = "bed")
-export(reduce(enhancerRegions), file.path(argv$o, "predictedEnhancers.bed"), format = "bed")
+export(promoterRegions, file.path(argv$o, "predictedPromoters.bed"), format = "bed")
+export(enhancerRegions, file.path(argv$o, "predictedEnhancers.bed"), format = "bed")
