909a042a39889df1ba0d652624a297465bfaf401
lonman99 <leon-schwartz@web.de>
2023-04-29 16:18:28 +0200

    filter enhancerAtlas regions before training

--- src/main/java/org/exbio/tfprio/steps/ehmm/CollapsePeakFiles.java
+++ src/main/java/org/exbio/tfprio/steps/ehmm/CollapsePeakFiles.java
@@ -7,7 +7,6 @@ import org.exbio.tfprio.configs.Configs;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.util.Collection;
 import java.util.HashMap;
